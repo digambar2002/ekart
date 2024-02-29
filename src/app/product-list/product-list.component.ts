74447f3df85ef25a6147e17b35998db3c0bdc3ee
digambar2002 <digambarchaudhari425303@gmail.com>
2024-02-29 17:06:21 +0530

    day 3 angular

---
+++ src/app/product-list/product-list.component.ts
@@ -0,0 +1,30 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'product-list',
+  templateUrl: './product-list.component.html',
+  styleUrl: './product-list.component.css'
+})
+export class ProductListComponent {
+
+  product = {
+    name: "iphone 13",
+    imgUrl: "https://cdn.myshoptet.com/usr/www.cesky-mobil.cz/user/shop/big/156955-4_apple-iphone-13.jpg",
+    price: 100,
+    color: "red",
+    discount: 14.33,
+    stock: 1,
+    
+  }
+  
+  getDiscount(){
+    return (this.product.price -(this.product.price * this.product.discount / 100))
+  }
+
+  setName(event: any){
+    
+    this.product.name = event.target.value;
+    
+  }
+
+}
